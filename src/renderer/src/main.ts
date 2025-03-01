e3902b53b0959f6deb1cd0098f4f20aae536a8f9
shinelith <lith2008@gmail.com>
2025-03-01 20:19:13 +0800

    加入PrimeVue的自定义主题

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -4,11 +4,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
+import { definePreset } from '@primeuix/themes';
+
+const MyPreset = definePreset(Aura,{
+  components: {
+    button:{
+      border:{
+        radius : "0px"
+      }
+    }
+  }
+})
 
 const app = createApp(App)
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: MyPreset,
     options: {
       cssLayer: {
         name: 'primevue',
