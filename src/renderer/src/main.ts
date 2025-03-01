924637b747d1dd8d1c6a995568ab2ca62c709e01
shinelith <lith2008@gmail.com>
2025-03-01 13:01:21 +0800

    添加PrimeVue

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,5 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura'; 
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            cssLayer: {
+                name: 'primevue',
+                order: 'theme, base, primevue'
+            }
+        }
+    }
+ });
+app.mount('#app')
