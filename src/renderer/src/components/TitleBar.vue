eff05d7a56ab7110c80c27d06cb3adebebc86141
shinelith <lith2008@gmail.com>
2025-03-04 23:21:39 +0800

    Titlebar

--- src/renderer/src/components/TitleBar.vue
+++ src/renderer/src/components/TitleBar.vue
@@ -9,32 +9,46 @@ const onDev = () => {
   window.api.onDevFunction()
 }
 
-// 最小化
-const onMinimize = () => {
-  window.api.onMinimize()
-}
-
 // 打开文件
 const onOpenFile = () => {
   window.api.openFile()
 }
 
-const count = ref(0)
+// 最小化
+const onWindowMin = () => {
+  this.$ipcRenderer.send('window-min')
+}
+
+// 最大化/恢复
+const onWindowMax = () => {
+  this.$ipcRenderer.send('window-max')
+}
+
+// 关闭app
+const onWindowClose = () => {
+  this.$ipcRenderer.send('window-close')
+}
 </script>
 
 <template>
   <div id="titlebar" class="flex">
-    <img class="m-5 h-6 w-6" src="../assets/app.svg" />
-    <Button @click="onOpenFile" label="打开" icon="pi pi-file" variant="text" />
-    <Button click="handleQ('request')" label="保存" icon="pi pi-save" variant="text" />
-    <Button label="打包" icon="pi pi-export" variant="text" />
-    <Button label="设置" icon="pi pi-cog" variant="text" />
-    <Button @click="onDev" label="DEV" />
-    <Button @click="onMinimize" label="最小化" />
-    <Button @click="" label="全屏" />
-    <Button @click="" label="退出" />
-    <div class="m-5">MizTran</div>
+    <div id="menu" class="flex flex-none">
+      <img class="m-6 h-6 w-6" src="../assets/app.svg" />
+      <Button @click="onOpenFile" label="打开" icon="pi pi-file" variant="text" />
+      <Button click="handleQ('request')" label="保存" icon="pi pi-save" variant="text" />
+      <Button label="打包" icon="pi pi-export" variant="text" />
+      <Button label="设置" icon="pi pi-cog" variant="text" />
+      <Button @click="onDev" label="MAIN DEV" />
+    </div>
+    <div id="apptitle" class="flex-auto m-6">{{ openedFile }}mizTranslator</div>
+    <div id="winctrl" class="flex-none">
+      <Button id="win_min" @click="onWindowMin" icon="" />
+      <Button id="win_max" @click="onWindowMax" icon="" />
+      <Button id="win_close" @click="onWindowClose" icon="" />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
