e356154a022e75261967d48da00c5d60fa3f7e45
shinelith <lith2008@gmail.com>
2025-03-10 21:44:58 +0800

    列表加入行单选

--- src/renderer/src/components/TextList.vue
+++ src/renderer/src/components/TextList.vue
@@ -1,16 +1,22 @@
 <script setup>
+import { ref } from 'vue'
 import { store } from '../store.js'
-
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 
-// defineProps({
-//   tableData: Array,
-// })
+const selectedProduct = ref();
+
+function onRowSelect(event){
+console.log('onRowSelect')
+}
+
+function onRowUnselect(event){
+  console.log('onRowUnselect')
+}
 </script>
 
 <template>
-  <DataTable class="h-full" :value="store.listdata" size="small" stripedRows resizableColumns>
+  <DataTable class="h-full" :value="store.listdata" @rowSelect="onRowSelect" @rowUnselect="onRowUnselect" dataKey="key" v-model:selection="selectedProduct" selectionMode="single" size="small" stripedRows resizableColumns>
     <Column field="key" header="标识" sortable class="max-w-10 truncate"></Column>
     <Column field="type" header="类型" sortable class="max-w-15 truncate"></Column>
     <Column field="originText" header="原文" class="min-w-20 max-w-50 truncate"></Column>
