e142d7e249b34ca7ffe7ce5594fc05f4e726858d
shinelith <lith2008@gmail.com>
2025-03-27 21:52:06 +0800

    公用数据整合

--- src/renderer/src/Preference.vue
+++ src/renderer/src/Preference.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref,onMounted } from 'vue'
-import { useStorage } from '@vueuse/core'
+import { settings } from './store.js'
 
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
@@ -8,15 +8,6 @@ import Textarea from 'primevue/textarea';
 import IftaLabel from 'primevue/IftaLabel'
 import Select from 'primevue/select'
 import Slider from 'primevue/slider'
-
-const settings = useStorage('ollama', {
-    system_prompt: '你是一位精通多种语言的专业翻译，能够准确地将内容翻译为中文。翻译时，请保留原文的语气、风格和表达方式。请遵守以下规则：1、专有名词（例如人名和地名）无需翻译，应保留其原形。2、仔细检查并确保译文流畅准确。3、回复前，请根据译文重新润色，确保与原文内容一致，既不增也不减任何内容，并使译文通俗易懂，符合目标语言的表达习惯。4、最终输出仅有润色后的译文，隐藏所有的过程和解释，整个输出应直接可引用，无需任何编辑。',
-    ollama_url: 'http://127.0.0.1:11434',
-    ollama_model: null,
-    ollama_model_list: null,
-    ollama_context_limit: 20,
-    ollama_temperature: 0.3
-})
 
 const ollamaModels = ref()
 // 刷新Ollama模型列表
