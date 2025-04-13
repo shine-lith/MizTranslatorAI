3685fa8ab952e746f30d164c92b6d1e1f7cc1a0b
shinelith <lith2008@gmail.com>
2025-04-13 23:33:07 +0800

    配置初始化

--- src/renderer/src/store.js
+++ src/renderer/src/store.js
@@ -26,16 +26,17 @@ export const store = reactive({
   // miz Dict数据
   listdata: null,
   // 任务信息
-  mission_data: null,
+  mission_data: null
 })
 
 export const settings = useStorage('settings', {
   overwrite: true,
   backup: false,
   translate_compare: false,
-  chat_prompt:'你是一个军事顾问，马上将要开展一个军事行动，之后会提供这次行动的必要信息。你需要结合这些信息，回答用户提出的问题并尽可能全面的提供已知信息。使用中文进行回答。以下是这次行动的信息：{miz_data}',
-  system_prompt:
-    '你是一位精通多种语言的专业翻译，能够准确地将内容翻译为中文。翻译时，请保留原文的语气、风格和表达方式。请遵守以下规则：1、专有名词（例如人名和地名）无需翻译，应保留其原形。2、仔细检查并确保译文流畅准确。3、回复前，请根据译文重新润色，确保与原文内容一致，既不增也不减任何内容，并使译文通俗易懂，符合目标语言的表达习惯。4、最终输出仅有润色后的译文，隐藏所有的过程和解释，整个输出应直接可引用，无需任何编辑。',
+  chat_prompt:
+    '你是一个军事顾问，马上将要开展一个军事行动，之后会提供这次行动的必要信息。你需要结合这些信息，回答用户提出的问题并尽可能全面的提供已知信息。使用中文进行回答。以下是这次行动的信息：{miz_data}',
+  translate_prompt:
+    '为一个军事模拟飞行游戏进行内容翻译，需要识别用户输入的内容，英文内容翻译为中文，直接返回翻译结果，不做额外解释。\r\n说话的人(:开头的)、人名、代号、呼号、编号保留英文\r\n人名、地名、代号、呼号、军事术语、缩写保持原文\r\n对话文本中，说话人的名字保持原文',
   ollama_host: 'http://127.0.0.1:11434',
   ollama_model: null,
   ollama_model_list: null,
